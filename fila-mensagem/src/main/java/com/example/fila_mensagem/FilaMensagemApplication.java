289a544c64b5d270b45efd7aac5ab2c22f36376f
André Brandão <andrebrandao12@yahoo.com>
2025-11-06 14:39:08 -0300

    Configuração do RabbitMQ

---
+++ fila-mensagem/src/main/java/com/example/fila_mensagem/FilaMensagemApplication.java
@@ -0,0 +1,13 @@
+package com.example.fila_mensagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilaMensagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilaMensagemApplication.class, args);
+	}
+
+}
---
+++ fila-mensagem/src/test/java/com/example/fila_mensagem/FilaMensagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fila_mensagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilaMensagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
